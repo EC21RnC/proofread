e3887e324fbbee2bc2e73f1bfcc14e2834403880
EC21RnC <93508908+EC21RnC@users.noreply.github.com>
2023-10-11 23:59:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import openai
 import os
 
 import undetected_chromedriver as uc
+from selenium import webdriver
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
