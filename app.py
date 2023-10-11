3a2db7bfa42ee52a558d47e762dd9e7fdabda4da
EC21RnC <93508908+EC21RnC@users.noreply.github.com>
2023-10-11 22:33:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import openai
 import os
-from selenium import webdriver
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -13,6 +13,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+@st.experimental_singleton
 def get_driver():
     return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
